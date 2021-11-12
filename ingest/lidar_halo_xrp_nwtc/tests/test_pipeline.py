89f3e8219cb093f0c2e01a161b12b702b5f2eba6
Maxwell Levin <maxwellevin@users.noreply.github.com>
2021-11-11 18:11:29 -0800

    Use better assertion method

--- ingest/lidar_halo_xrp_nwtc/tests/test_pipeline.py
+++ ingest/lidar_halo_xrp_nwtc/tests/test_pipeline.py
@@ -6,7 +6,6 @@ from ingest.lidar_halo_xrp_nwtc import Pipeline
 parent = os.path.dirname(__file__)
 
 
-# TODO – Developer: Update paths to your input files here. Please add tests if needed.
 def test_pipeline_at_nwtc():
     set_dev_env()
     pipeline = Pipeline(
@@ -21,5 +20,4 @@ def test_pipeline_at_nwtc():
             "tests/data/expected/nwtc/nwtc.lidar-halo_xrp.b0.20210510.000125.nc", parent
         )
     )
-
-    assert output.equals(expected)
+    xr.testing.assert_allclose(output, expected)
