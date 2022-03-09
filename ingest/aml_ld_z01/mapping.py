412405452e08630def65f3d1da966824f06d708d
Maxwell Levin <maxwellevin@users.noreply.github.com>
2022-03-09 11:20:48 -0800

    Pattern matches the start of a string

--- ingest/aml_ld_z01/mapping.py
+++ ingest/aml_ld_z01/mapping.py
@@ -10,20 +10,19 @@ from . import Pipeline
 # match the directory structure in addition to (or instead of) the file basename.
 mapping: Dict["AnyStr@compile", IngestSpec] = {
     # Mapping for Raw Data -> Ingest
-#     re.compile(r"aml\.ld\.z01\.0.*\.dat"): IngestSpec(
-    re.compile(r".*"): IngestSpec(
+    re.compile(r".*aml\.ld\.z01\.0.*\.dat"): IngestSpec(
         pipeline=Pipeline,
         pipeline_config=expand("config/pipeline_config.yml", __file__),
         storage_config=expand("config/storage_config.yml", __file__),
         name="aml_ld_z01",
     ),
     # Mapping for Processed Data -> Ingest (so we can reprocess plots)
-#     re.compile(r"aml\.ld\.z01\.a.*\.dat"): IngestSpec(
-#         pipeline=Pipeline,
-#         pipeline_config=expand("config/pipeline_config.yml", __file__),
-#         storage_config=expand("config/storage_config.yml", __file__),
-#         name="plot_aml_ld_z01",
-#     ),
+    re.compile(r".*aml\.ld\.z01\.a.*\.dat"): IngestSpec(
+        pipeline=Pipeline,
+        pipeline_config=expand("config/pipeline_config.yml", __file__),
+        storage_config=expand("config/storage_config.yml", __file__),
+        name="plot_aml_ld_z01",
+    ),
     # You can add as many {regex: IngestSpec} entries as you would like. This is useful
     # if you would like to reuse this ingest at other locations or possibly for other
     # similar instruments
