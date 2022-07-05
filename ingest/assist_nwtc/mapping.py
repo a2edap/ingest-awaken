7f597a039f0190dfe1126dad3f18230a01f62290
StefanoWind <stefano.letizia@nrel.gov>
2022-07-05 15:38:20 -0600

    finalized file skip, assist differentiated storage

--- ingest/assist_nwtc/mapping.py
+++ ingest/assist_nwtc/mapping.py
@@ -9,11 +9,13 @@ from . import Pipeline
 # full filepath will be passed to the compiled regex pattern, so you can optionally
 # match the directory structure in addition to (or instead of) the file basename.
 mapping: Dict["AnyStr@compile", IngestSpec] = {
+    # Mapping for Raw Data -> Ingest (DAP)
+    re.compile(r".*assist\.z\d{2}.00.\d{8}\.\d{6}\.assistsummary\.cdf"): IngestSpec(
         pipeline=Pipeline,
         pipeline_config=expand("config/pipeline_config_nwtc.yml", __file__),
         storage_config=expand("config/storage_config.yml", __file__),
         name="assist_summary_DAP",
-    ),
+    ),   
     # Mapping for Raw Data -> Ingest (NREL Server)
     re.compile(r".*Assist-10.*assistsummary.*.cdf"): IngestSpec(
         pipeline=Pipeline,
@@ -34,7 +36,7 @@ mapping: Dict["AnyStr@compile", IngestSpec] = {
         name="assist_summary_12",
     ),
     # Mapping for Processed Data -> Ingest (so we can reprocess plots)
-    re.compile(r".*assist.*b0.*nc"): IngestSpec(
+    re.compile(r"YOUR-REGEX-HERE"): IngestSpec(
         pipeline=Pipeline,
         pipeline_config=expand("config/pipeline_config_nwtc.yml", __file__),
         storage_config=expand("config/storage_config.yml", __file__),
